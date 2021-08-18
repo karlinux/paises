077d7f774edbd4b5fc764e4e0e48bdba940041a2
Carlos Cruz <ccruzlo@veracruz.gob.mx>
2021-08-18 14:43:24 -0500

    first commit

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: []
+})
+
+
+export class PorPaisComponent {
+  termino = 'Hola mundo';
+
+  constructor() { }
+
+  buscar(){
+    
+  }
+}
+
